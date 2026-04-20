f8dfcf5f110e7495e4eca578b8c062f01e28a020
Bueltan <denisbueltan@gmail.com>
2026-04-20 07:44:44 -0300

    improve mic btn

--- src/ui_start.cpp
+++ src/ui_start.cpp
@@ -1,37 +1,131 @@
 #include "ui_start.h"
 
 #include <TFT_eSPI.h>
+#include <WiFi.h>
 
 #include "display_hal.h"
 #include "app_runtime_state.h"
 #include "app_state.h"
-#include <WiFi.h>
+
 static TFT_eSPI_Button btnMic;
 static volatile UiState uiState = UI_READY;
 
+static constexpr int MAIN_Y_OFFSET = 15;
+
 static constexpr int SETUP_X = 188;
-static constexpr int SETUP_Y = 14;
+static constexpr int SETUP_Y = 14 + MAIN_Y_OFFSET;
 static constexpr int SETUP_W = 36;
 static constexpr int SETUP_H = 36;
 
-static const uint8_t setupIcon16x16[] = {
-    0b00000111, 0b11100000,
-    0b00001100, 0b00110000,
-    0b00011000, 0b00011000,
-    0b00110011, 0b11001100,
-    0b00100111, 0b11100100,
-    0b01100110, 0b01100110,
-    0b01101100, 0b00110110,
-    0b11111100, 0b00111111,
-    0b11111100, 0b00111111,
-    0b01101100, 0b00110110,
-    0b01100110, 0b01100110,
-    0b00100111, 0b11100100,
-    0b00110011, 0b11001100,
-    0b00011000, 0b00011000,
-    0b00001100, 0b00110000,
-    0b00000111, 0b11100000};
+static constexpr int MIC_BTN_CX = 120;
+static constexpr int MIC_BTN_CY = 235 + MAIN_Y_OFFSET;
+static constexpr int MIC_BTN_W = 140;
+static constexpr int MIC_BTN_H = 78;
 
+static const uint8_t micIcon32x32[] = {
+    0x00, 0x07, 0xE0, 0x00, //         ██████
+    0x00, 0x1F, 0xF8, 0x00, //       ██████████
+    0x00, 0x3F, 0xFC, 0x00, //      ████████████
+    0x00, 0x7F, 0xFE, 0x00, //     ██████████████
+    0x00, 0x78, 0x1E, 0x00, //     ████      ████
+    0x00, 0x78, 0x1E, 0x00, //     ████      ████
+    0x00, 0x78, 0x1E, 0x00, //     ████      ████
+    0x00, 0x78, 0x1E, 0x00, //     ████      ████
+    0x00, 0x78, 0x1E, 0x00, //     ████      ████
+    0x00, 0x78, 0x1E, 0x00, //     ████      ████
+    0x00, 0x78, 0x1E, 0x00, //     ████      ████
+    0x00, 0x78, 0x1E, 0x00, //     ████      ████
+    0x00, 0x7F, 0xFE, 0x00, //     ██████████████
+    0x00, 0x3F, 0xFC, 0x00, //      ████████████
+    0x00, 0x1F, 0xF8, 0x00, //       ██████████
+    0x00, 0x07, 0xE0, 0x00, //         ██████
+    0x00, 0x07, 0xE0, 0x00,
+    0x00, 0x07, 0xE0, 0x00,
+    0x00, 0x07, 0xE0, 0x00,
+    0x00, 0x07, 0xE0, 0x00,
+    0x00, 0x1F, 0xF8, 0x00, //       ██████████
+    0x00, 0x3F, 0xFC, 0x00, //      ████████████
+    0x00, 0x07, 0xE0, 0x00, //         ██████
+    0x00, 0x07, 0xE0, 0x00,
+    0x00, 0x0F, 0xF0, 0x00, //        ████████
+    0x00, 0x1C, 0x38, 0x00, //       ███    ███
+    0x00, 0x38, 0x1C, 0x00, //      ███      ███
+    0x00, 0x70, 0x0E, 0x00, //     ███        ███
+    0x00, 0x60, 0x06, 0x00, //     ██          ██
+    0x00, 0x7F, 0xFE, 0x00, //     ██████████████
+    0x00, 0x3F, 0xFC, 0x00, //      ████████████
+    0x00, 0x00, 0x00, 0x00};
+    
+static const uint8_t speakerIcon32x32[] = {
+    0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00,
+    0x00,0x03,0x00,0x00,
+    0x00,0x07,0x80,0x00,
+    0x00,0x0F,0xC0,0x60,
+    0x00,0x1F,0xE0,0xF0,
+    0x00,0x3F,0xF0,0xF8,
+    0x00,0x7F,0xF8,0x7C,
+    0x00,0xFF,0xFC,0x1E,
+    0x01,0xFF,0xFE,0x0E,
+    0x03,0xFF,0xFF,0x06,
+    0x07,0xFF,0xFF,0x06,
+    0x0F,0xFF,0xFF,0x06,
+    0x1F,0xFF,0xFF,0x06,
+    0x1F,0xFF,0xFF,0x06,
+    0x0F,0xFF,0xFF,0x06,
+    0x07,0xFF,0xFF,0x06,
+    0x03,0xFF,0xFF,0x06,
+    0x01,0xFF,0xFE,0x0E,
+    0x00,0xFF,0xFC,0x1E,
+    0x00,0x7F,0xF8,0x7C,
+    0x00,0x3F,0xF0,0xF8,
+    0x00,0x1F,0xE0,0xF0,
+    0x00,0x0F,0xC0,0x60,
+    0x00,0x07,0x80,0x00,
+    0x00,0x03,0x00,0x00,
+    0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00
+};
+
+static const uint8_t xIcon16x16[] = {
+    0b11000000, 0b00000011,
+    0b01100000, 0b00000110,
+    0b00110000, 0b00001100,
+    0b00011000, 0b00011000,
+    0b00001100, 0b00110000,
+    0b00000110, 0b01100000,
+    0b00000011, 0b11000000,
+    0b00000001, 0b10000000,
+    0b00000001, 0b10000000,
+    0b00000011, 0b11000000,
+    0b00000110, 0b01100000,
+    0b00001100, 0b00110000,
+    0b00011000, 0b00011000,
+    0b00110000, 0b00001100,
+    0b01100000, 0b00000110,
+    0b11000000, 0b00000011};
+static const uint8_t setupIcon16x16[] = {
+    0b00000111, 0b11100000, //     ███████
+    0b00001111, 0b11110000, //    █████████
+    0b00011000, 0b00011000, //   ██      ██
+    0b00110011, 0b11001100, //  ██  ████  ██
+    0b00100111, 0b11100100, //  █  ███████  █
+    0b01100110, 0b01100110, // ██  ██  ██  ██
+    0b01101100, 0b00110110, // ██ ██    ██ ██
+    0b11111100, 0b00111111, // ███████  ███████
+    0b11111100, 0b00111111, // ███████  ███████
+    0b01101100, 0b00110110, // ██ ██    ██ ██
+    0b01100110, 0b01100110, // ██  ██  ██  ██
+    0b00100111, 0b11100100, //  █  ███████  █
+    0b00110011, 0b11001100, //  ██  ████  ██
+    0b00011000, 0b00011000, //   ██      ██
+    0b00001111, 0b11110000, //    █████████
+    0b00000111, 0b11100000  //     ███████
+};
 static const char *uiStateToButtonLabel(UiState state)
 {
     switch (state)
@@ -57,7 +151,7 @@ static void drawTitle()
     screenLock();
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
     tft.setTextSize(2);
-    tft.setCursor(18, 22);
+    tft.setCursor(18, 22 + MAIN_Y_OFFSET);
     tft.print("HAND-WALL-E");
     screenUnlock();
 }
@@ -143,18 +237,18 @@ static void drawStatusText(const char *line1, const char *line2 = nullptr)
     TFT_eSPI &tft = display();
 
     screenLock();
-    tft.fillRect(10, 70, 220, 90, TFT_BLACK);
-    tft.drawRect(10, 70, 220, 90, TFT_GREEN);
+    tft.fillRect(10, 70 + MAIN_Y_OFFSET, 220, 90, TFT_BLACK);
+    tft.drawRect(10, 70 + MAIN_Y_OFFSET, 220, 90, TFT_GREEN);
 
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
     tft.setTextSize(2);
-    tft.setCursor(18, 88);
+    tft.setCursor(18, 88 + MAIN_Y_OFFSET);
     tft.print(line1 ? line1 : "");
 
     if (line2)
     {
         tft.setTextSize(1);
-        tft.setCursor(18, 126);
+        tft.setCursor(18, 126 + MAIN_Y_OFFSET);
         tft.print(line2);
     }
 
@@ -165,15 +259,45 @@ static void drawMicButton(bool inverted = false)
 {
     TFT_eSPI &tft = display();
 
+    const uint16_t bg = inverted ? TFT_GREEN : TFT_BLACK;
+    const uint16_t fg = inverted ? TFT_BLACK : TFT_GREEN;
+
+    const int x = MIC_BTN_CX - MIC_BTN_W / 2;
+    const int y = MIC_BTN_CY - MIC_BTN_H / 2;
+
+    const uint8_t *mainIcon = micIcon32x32;
+    int mainIconW = 32;
+    int mainIconH = 32;
+
+    if (uiState == UI_SPEAKING)
+    {
+        mainIcon = speakerIcon32x32;
+    }
+
     screenLock();
+
     btnMic.initButton(
         &tft,
-        120, 235,
-        140, 78,
+        MIC_BTN_CX, MIC_BTN_CY,
+        MIC_BTN_W, MIC_BTN_H,
         TFT_GREEN, TFT_BLACK, TFT_GREEN,
-        (char *)uiStateToButtonLabel(uiState),
+        (char *)"",
         2);
-    btnMic.drawButton(inverted);
+
+    tft.fillRoundRect(x, y, MIC_BTN_W, MIC_BTN_H, 10, bg);
+    tft.drawRoundRect(x, y, MIC_BTN_W, MIC_BTN_H, 10, TFT_GREEN);
+
+    const int iconX = MIC_BTN_CX - mainIconW / 2;
+    const int iconY = MIC_BTN_CY - mainIconH / 2;
+    tft.drawBitmap(iconX, iconY, mainIcon, mainIconW, mainIconH, fg);
+
+    if (uiState == UI_ERROR)
+    {
+        const int xIconX = MIC_BTN_CX + 10;
+        const int xIconY = MIC_BTN_CY - 20;
+        tft.drawBitmap(xIconX, xIconY, xIcon16x16, 16, 16, fg);
+    }
+
     screenUnlock();
 }
 
@@ -231,6 +355,7 @@ void uiStartApplyState(UiState state)
             drawStatusText(lastErrorLine1.c_str(), lastErrorLine2.c_str());
         }
 
+        drawMicButton(state == UI_LISTENING);
         return;
     }
 
@@ -260,7 +385,7 @@ void uiStartApplyState(UiState state)
         break;
     }
 
-    drawMicButton(false);
+    drawMicButton(state == UI_LISTENING);
     drawSetupButton(false);
     drawWifiStatus();
 }
